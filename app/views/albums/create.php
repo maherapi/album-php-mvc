efe99db0f1bd29601e3ad663e9dccf4f044c88cc
maherapi <maher.azkoul@gmail.com>
2020-06-30 11:36:15 +0300

    UI changes in create new album cover image preview view

--- app/views/albums/create.php
+++ app/views/albums/create.php
@@ -63,7 +63,7 @@
         </div>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">OK</button>
       </div>
     </div>
   </div>
